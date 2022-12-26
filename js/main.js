9dd090e454eeaa7cb8b0a10771f4bd405cda57b3
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-26 14:07:46 +0500

    simple commit

--- js/main.js
+++ js/main.js
@@ -1,98 +1,46 @@
 const elForm = document.querySelector(".js-form");
 
-const elList = document.querySelector(".js-list");
+const elList = document.querySelector(".contact-book");
 const elName = document.querySelector(".js-name");
 const elSelect = document.querySelector(".js-select");
-const elPhone = document.querySelector(".js-number");
+const elNumber = document.querySelector(".js-number");
 
-const localData = JSON.parse(window.localStorage.getItem("List"));
-const todos = localData || [];
 
-let renderTodo = (arr, node) => {
-  window.localStorage.setItem("List", JSON.stringify(todos));
-  node.innerHTML = "";
-  arr.forEach((el) => {
-    let newTodoName = document.createElement("h3");
+// const localData = JSON.parse(window.localStorage.getItem("List"));
+// const todos = localData || [];
 
-    let relationTxt = document.createElement("h4");
-    let PhoneNum = document.createElement("p");
+elForm.addEventListener("submit",(evt)=>{
+  evt.preventDefault()
+  elSelect.addEventListener("change",(evt)=>{
+    evt.preventDefault()
+    console.log(elSelect.value);
+  })
 
-    let editButton = document.createElement("button");
-    editButton.setAttribute("class", "btn btn-warning ms-auto js-edit-btn");
-    let deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "btn btn-danger ms-1 js-delete-btn");
-    editButton.textContent = "Edit";
-    deleteButton.textContent = "Delete";
-    deleteButton.dataset.todoId = el.id;
-    editButton.dataset.todoId = el.id;
+  const elDiv =document.createElement('div')
+  const elItem =document.createElement('h4')
+  const elPhone =document.createElement('p')
+  const elSpan =document.createElement('span')
+  elDiv.setAttribute('class','text-center border border-info border-start-0 rounded-end contact-div')
+  elItem.setAttribute('class','list-item')
+  elSpan.setAttribute('class','list-item')
+  elPhone.setAttribute('class','list-item')
+  
+ elList.appendChild(elDiv) 
+elDiv.appendChild(elItem)
+elDiv.appendChild(elSpan)
+elDiv.appendChild(elPhone)
 
-    elList.appendChild(newTodoName);
 
-    newTodoName.innerHTML = "Name: " + el.name;
-    relationTxt.innerHTML = "Relation: " + el.relation;
-    PhoneNum.innerHTML = "Phone number: " + el.phone;
+elItem.innerHTML =elName.value
+elPhone.innerHTML =elSelect.value
+elSpan.innerHTML =elNumber.value
 
-    elList.appendChild(relationTxt);
-    elList.appendChild(PhoneNum);
-    elList.appendChild(editButton);
-    elList.appendChild(deleteButton);
+  console.log(elName.value);
+  console.log(elSelect.value);
+})
 
-    elName.value = "";
-    elPhone.value = "";
-    elSelect.value = "";
-  });
-};
 
-elForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  const findedIndex = todos.findIndex((el) => el.phone === elPhone.value);
-  console.log(findedIndex);
-  if (findedIndex >= 0) {
-    alert("There is a user of such a number");
-  } else {
-    let elNameVal = elName.value;
-    let elPhoneVal = elPhone.value;
-    let elSelectVal = elSelect.value;
-
-    const todo = {
-      id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 1,
-      name: elNameVal,
-      relation: elSelectVal,
-      phone: elPhoneVal,
-      isCompleted: false,
-    };
-    todos.push(todo);
-    renderTodo(todos, elList);
-  }
-});
-
-if (todos.length != 0) {
-  renderTodo(todos, elList);
-}
-
-elList.addEventListener("click", (evt) => {
-  if (evt.target.matches(".js-delete-btn")) {
-    let listId = evt.target.dataset.todoId;
-    let findedList = todos.findIndex((el) => el.id == listId);
-    todos.splice(findedList, 1);
-    renderTodo(todos, elList);
-  }
-  if (evt.target.matches(".js-edit-btn")) {
-    let listId = evt.target.dataset.todoId;
-
-    let findedList = todos.find((el) => el.id == listId);
-    let editName = prompt("edit name", findedList.name);
-    let editRelation = prompt("edit relationship", findedList.relation);
-    let editPhone = prompt("edit phone", findedList.phone);
-
-    findedList.name = editName;
-    findedList.relation = editRelation;
-    findedList.phone = editPhone;
-
-    renderTodo(todos, elList);
-  }
-});
+//   window.localStorage.setItem("List", JSON.stringify(todos));
 
 let ModeBtn = document.querySelector(".mode");
 let theme = false;
@@ -113,11 +61,4 @@ let ChageMode = () => {
 ChageMode();
 
 
-// const func = () => {
-//     var element = document.body;
-//     console.log(element);
-//     element.classList.toggle("dark-mode");
-// }
-// const localVal =localStorage.setItem('dark',JSON.stringify(element))
 
-// console.log(element);
